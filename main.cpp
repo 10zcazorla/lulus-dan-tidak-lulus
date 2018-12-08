de3890bf10229099034ee4a3fc216300c5641adc
M. Zainur Ramdhan <45529730+10zcazorla@users.noreply.github.com>
2018-12-08 07:18:01 +0700

    Add files via upload
    #include <iostream>
    #include <conio.h>
    
    using namespace std;
    
    int main()
    {
    
        cout << "Masukan Nilai Tugas Anda: ";
        cin>>"nilai: ";
        if (nilai<75) {
            cout<<"\nAnda Tidak Lulus\n";
        } else {
        cout<<"\nSelamat Anda Lulus\n";
        }
        }
        getc() ;
        return 0 ;
    
    }

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+int main()
+{
+
+    cout << "Masukan Nilai Tugas Anda: ";
+    cin>>"nilai: ";
+    if (nilai<75) {
+        cout<<"\nAnda Tidak Lulus\n";
+    } else {
+    cout<<"\nSelamat Anda Lulus\n";
+    }
+    }
+    getc() ;
+    return 0 ;
+
+}
